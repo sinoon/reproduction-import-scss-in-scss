c449d776b9da78a39ae57269e6d36fca2fe55ea9
sinoon <sinoon1218@gmail.com>
2022-03-04 23:05:24 +0800

    feat: add file to reproduction fail in import css in scss

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    // This starter template is using Vue 3 <script setup> SFCs
+    // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+    import HelloWorld from './components/HelloWorld.vue'
+    // import './styles/index.less'
+</script>
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style lang="less">
+    @import './styles/index.scss';
+</style>
